78d9c058e43f3963cc1e79a1d4997440b5879e7a
Leah Gerald <Leanator13@gmail.com>
2017-12-12 16:38:59 -0500

    basic routes good

--- server/api/pokemon.js
+++ server/api/pokemon.js
@@ -3,7 +3,15 @@ const {db, Pokemon} = require('../models')
 
 //GET
 //all the pokemans
+pokeRouter.get("/", (req,res,next) => {
+    Pokemon.findAll(req.body)
+    .then(pokemans => res.status(200).json(pokemans))
+})
 //one pokeman
+pokeRouter.get("/:id", (req,res,next) => {
+    Pokemon.findById(req.params.id)
+    .then(pokemon => res.json(pokemon))
+})
 
 
 
--- server/api/region.js
+++ server/api/region.js
@@ -3,7 +3,15 @@ const {db, Region} = require('../models')
 
 //GET
 //all the regions
+regionRouter.get("/", (req,res,next) => {
+    Region.findAll(req.body)
+    .then(regions => res.status(200).json(regions))
+})
 //one region
+regionRouter.get("/:id", (res,req,next) => {
+    Region.findById(req.params.id)
+    .then(region => res.status(200).json(region))
+})
 
 
 module.exports = regionRouter
